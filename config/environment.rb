a3973303c35dbfae4c7f30a05da258ba8759c583
EC2 Default User <vovandangkhoa@gmail.com>
2020-08-11 01:43:13 +0000

    clean up layout

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Rails Validation message don't make change style of page
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
