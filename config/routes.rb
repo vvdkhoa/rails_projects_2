b41d59a8ffa70173efe35fe942fb7a2102a56e94
EC2 Default User <vovandangkhoa@gmail.com>
2020-08-11 17:55:09 +0900

    Add user creation action

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
     
     # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
     resources :articles
+    
+    get 'signup', to: 'users#new'
+    resources :users, except: [:new]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_063520) do
+ActiveRecord::Schema.define(version: 2020_08_11_073558) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_063520) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
