ff3c0a0203a05744448be639281965187dbd1690
vvdkhoa <vovandangkhoa@gmail.com>
2020-08-14 10:50:57 +0900

    add admin authority, admin user can edit, delete all articles, can delete another user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
-    def create
+    def create 
+        @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id # Login session
             flash[:notice] = "Welcome to the Yodo Blog #{@user.username}, you have successfully signup"
@@ -43,7 +44,7 @@ class UsersController < ApplicationController
     
     def destroy
         @user.destroy
-        session[:user_id] = nil # Log out, clear cookies
+        session[:user_id] = nil if @user == current_user # Log out, clear cookies except when admin delete another account
         flash[:notice] = "Account and all associated articles successfully deleted"
         redirect_to articles_path
     end
@@ -67,11 +68,12 @@ class UsersController < ApplicationController
     # end
     
     def require_same_user
-        if current_user != @user
-            flash[:alert] = "You can only edit your own account"
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own account"
             redirect_to @user
         end
         
     end
+    
     
 end
--- db/migrate/20200808142702_remove_columns.rb
+++ db/migrate/20200808142702_remove_columns.rb
@@ -1,6 +1,6 @@
 class RemoveColumns < ActiveRecord::Migration[6.0]
   def change
-    remove_column :articles, :update_at
-    remove_column :articles, :create_at
+    # remove_column :articles, :update_at
+    # remove_column :articles, :create_at
   end
 end
